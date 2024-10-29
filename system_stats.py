8539e8e0f37787bfbc02a2b4c59e522e68ab02f7
nikkkittta <nikitka.konstantinov.85@gmail.com>
2024-10-29 20:21:42 +0300

    Добавлен скрипт определения сетевых интерфейсов и их параметров. Созданы файлы для Jupyter и скрипт .py

---
+++ system_stats.py
@@ -0,0 +1,22 @@
+import psutil                                       #библиотека для работы с системными данными
+
+interfaces = psutil.net_if_stats()                  #записываем данные о состоянии всех сетевых интерфейсов
+
+for interface, stats in interfaces.items():         #перебираем данные и выводим их
+
+    print(f"Интерфейс: {interface}")                #название интерфейса
+
+    print(f"  Активен: {stats.isup}")               #состояние интерфейса
+
+    print(f"Скорость:  {stats.speed} Мбит/с")       #скорость соединения
+
+    if stats.duplex == 2:
+        duplex_mode = 'Duplex'
+    elif stats.duplex == 1:
+        duplex_mode = 'Half-duplex'
+    else:
+        duplex_mode = 'Unavailable'
+
+    print(f"Duplex mode: {duplex_mode} ")           #режим работы интерфейса
+
+    print(f" MTU: {stats.mtu}\n")                   #максимальный размер пакета
